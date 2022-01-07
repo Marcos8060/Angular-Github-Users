f78e30ef8b5499676a4ef23e9b01d05052d0948e
Marcos Ochieng <marcosgav80@gmail.com>
2022-01-07 17:43:33 +0300

    added the github profile details via string interpolation

--- src/app/github-profile-profile/github-profile-profile.component.ts
+++ src/app/github-profile-profile/github-profile-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-github-profile-profile',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-profile-profile.component.css']
 })
 export class GithubProfileProfileComponent implements OnInit {
+  @Input() githubProfile: any;
 
   constructor() { }
 
