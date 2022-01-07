c5e39f969952016baa5e10f954ff32eb416ac549
Marcos Ochieng <marcosgav80@gmail.com>
2022-01-07 16:59:23 +0300

    added the profile url

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { client_id, client_secret } from './github/clientsecret';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ export class GithubService {
   constructor(httpClient: HttpClientModule) { }
 
   // get github profile
-
+  getProfile(userInput:string){
+    let profileURL = `https://api.github.com/users/${userInput}?client_id=${client_id}&client_secret=${client_secret}`;
+    
+  }
   // get github repos
 }
